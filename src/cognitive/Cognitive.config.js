ed9b39097be2d4f9aaf7d1785ed7af84a97faf25
Panepo <whisper.ko@gmail.com>
2018-12-25 13:42:53 +0800

    update componment
    update componment

--- src/cognitive/Cognitive.config.js
+++ src/cognitive/Cognitive.config.js
@@ -1,12 +1,11 @@
 export const configVision = {
-  apiKey: 'e11462f1cef8405f966bfbe8e7789de2',
+  apiKey: 'your api key',
   url: 'https://eastasia.api.cognitive.microsoft.com/vision/v1.0/analyze?'
 }
 
 export const configText = {
-  apiKey: '94e14770ae5742a08d89a5b8924e375b',
+  apiKey: 'your api key',
   azure:
     'https://eastasia.api.cognitive.microsoft.com/text/analytics/v2.0/keyPhrases',
-  docker:
-    'https://cors-anywhere.herokuapp.com/http://localhost:5000/text/analytics/v2.0/keyPhrases'
+  docker: 'http://localhost:5000/text/analytics/v2.0/keyPhrases'
 }
