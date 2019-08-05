617c6c8e4204ac24fffd2e758eab41e41e6f8960
Panepo <whisper.ko@gmail.com>
2019-08-05 16:32:04 +0800

    add python
    add python scripts

---
+++ python/text_detect.py
@@ -0,0 +1,20 @@
+# Import required modules
+import argparse
+from langdetect import detect
+
+############ Add argument parser for command line arguments ############
+parser = argparse.ArgumentParser(description="Detect input language")
+parser.add_argument(
+    "--input",
+    type=str,
+    required=True,
+    help="Input string for analysis",
+)
+args = parser.parse_args()
+
+def main():
+    lang = detect(args.input)
+    print("[INFO] Language detected: " + lang)
+
+if __name__ == "__main__":
+    main()
---
+++ python/text_jieba.py
@@ -0,0 +1,20 @@
+# Import required modules
+import argparse
+import jieba
+
+############ Add argument parser for command line arguments ############
+parser = argparse.ArgumentParser(description="Segmentate input chinese language")
+parser.add_argument(
+    "--input",
+    type=str,
+    required=True,
+    help="Input string for segmentation",
+)
+args = parser.parse_args()
+
+def main():
+    seg_list = jieba.cut(args.input)
+    print("[INFO] Segmentation results: " + "/".join(seg_list))
+
+if __name__ == "__main__":
+    main()
