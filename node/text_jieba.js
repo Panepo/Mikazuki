78852af94ba557657551e05184acd5ec39b67246
Panepo <whisper.ko@gmail.com>
2019-08-13 17:39:07 +0800

    update node
    update text_jieba

--- node/text_jieba.js
+++ node/text_jieba.js
@@ -13,14 +13,30 @@ if (!input) {
 // nodejieba only support Simplified Chinese, so translate Traditonal to Simplified.
 // After segment, translate back to Traditonal Chinese.
 function run(input) {
-  // process jieba
-  const words = nodejieba.cut(trans.sify(input))
-  const results = []
+  const sifyInput = trans.sify(input)
+  let words = nodejieba.cut(sifyInput)
+  const resultCut = []
   words.forEach(word => {
-    results.push(trans.tify(word))
+    resultCut.push(trans.tify(word))
   })
   console.log("[INFO] the jieba cut results are:")
-  console.info(results)
+  console.info(resultCut)
+
+  words = nodejieba.extract(sifyInput, 5)
+  const resultExtract = []
+  words.map(word => {
+    resultExtract.push(trans.tify(word.word))
+  })
+  console.log("[INFO] the jieba extract results are:")
+  console.info(resultExtract)
+
+  words = nodejieba.tag(sifyInput)
+  const resultTag = []
+  words.map(word => {
+    resultTag.push(trans.tify(word.word) + " (" + word.tag + ")")
+  })
+  console.log("[INFO] the jieba extract results are:")
+  console.info(resultTag)
 }
 
 run(input)
