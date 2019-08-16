e7e1c21cc2a17ede21ec3c74037f9918a58428b6
Panepo <whisper.ko@gmail.com>
2019-08-16 15:55:18 +0800

    update node
    update text_jieba

--- server/src/helpers/keyword.helper.js
+++ server/src/helpers/keyword.helper.js
@@ -1,27 +1,21 @@
 import natural from 'natural'
 
-const ThresholdJaroWinkler = 0.8
-
-export const isWeather = tokens => {
+export const isKeyword = (tokens, wordsCht, wordsEng, threshold) => {
   let result = false
   tokens.map(token => {
     if (token.tag === 'n') {
-      const keywords = ["天氣", "氣溫", "溫度", "壓力", "氣壓", "濕度", "溼度", "風", "雨量"]
-      keywords.forEach(word => {
+      wordsCht.forEach(word => {
         if (token.word === word) {
           result = true
         }
       })
     } else if (token.tag === 'eng') {
-      const keywords = [
-        "weather", "temperature", "wind", "humidity", "pressure", "precipitation"
-      ]
-      keywords.forEach(word => {
+      wordsEng.forEach(word => {
         if (token.word === word) {
           result = true
         }
         // eslint-disable-next-line
-        if (natural.JaroWinklerDistance(token.word, word) > ThresholdJaroWinkler) {
+        if (natural.JaroWinklerDistance(token.word, word) > threshold) {
           result = true
         }
         // console.log(token.word + " & " + word + " : " + natural.JaroWinklerDistance(token.word, word))
@@ -29,4 +23,13 @@ export const isWeather = tokens => {
     }
   })
   return result
+}
+
+export const isWeather = tokens => {
+  const wordsCht = ["天氣", "氣溫", "溫度", "壓力", "氣壓", "濕度", "溼度", "風", "雨量"]
+  const wordsEng = [
+    "weather", "temperature", "wind", "humidity", "pressure", "precipitation"
+  ]
+  const thre = 0.8
+  return isKeyword(tokens, wordsCht, wordsEng, thre)
 }
